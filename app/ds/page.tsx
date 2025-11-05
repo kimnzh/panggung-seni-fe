104d6f9703149f7e95fc8d464b6670ffd7ac53c8
Hakim Nizami <hakimnizami15@gmail.com>
2025-11-06 00:15:57 +0700

    ds: implemented radio buttons and checkboxes + added metadata

--- app/ds/page.tsx
+++ app/ds/page.tsx
@@ -5,10 +5,11 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { Label } from "@/components/ui/label";
+import { Checkbox } from "@/components/ui/checkbox";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import AppsIcon from "@mui/icons-material/Apps";
 
 export default function DesignSystemPage() {
@@ -41,6 +42,40 @@ export default function DesignSystemPage() {
             <DropdownMenuItem variant="outline">Choice 4</DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
+      </div>
+
+      {/* Radio Buttons & Checkboxes */}
+      <div className="flex gap-8">
+        <RadioGroup defaultValue="comfortable">
+          <div className="flex items-center gap-3">
+            <RadioGroupItem value="default" id="r1" />
+            <Label htmlFor="r1">Default</Label>
+          </div>
+          <div className="flex items-center gap-3">
+            <RadioGroupItem value="comfortable" id="r2" />
+            <Label htmlFor="r2">Comfortable</Label>
+          </div>
+          <div className="flex items-center gap-3">
+            <RadioGroupItem value="compact" id="r3" />
+            <Label htmlFor="r3">Compact</Label>
+          </div>
+        </RadioGroup>
+        <div>
+          <div className="flex flex-col gap-3">
+            <div className="flex items-center gap-3">
+              <Checkbox id="default" />
+              <Label htmlFor="default">Default</Label>
+            </div>
+            <div className="flex items-center gap-3">
+              <Checkbox id="comfortable" />
+              <Label htmlFor="comfortable">Comfortable</Label>
+            </div>
+            <div className="flex items-center gap-3">
+              <Checkbox id="compact" />
+              <Label htmlFor="compact">Compact</Label>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
--- components/ui/checkbox.tsx
+++ components/ui/checkbox.tsx
@@ -1,10 +1,10 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import * as CheckboxPrimitive from "@radix-ui/react-checkbox"
-import { CheckIcon } from "lucide-react"
+import * as React from "react";
+import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
+import { CheckIcon } from "lucide-react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 function Checkbox({
   className,
@@ -14,19 +14,20 @@ function Checkbox({
     <CheckboxPrimitive.Root
       data-slot="checkbox"
       className={cn(
-        "peer border-input dark:bg-input/30 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground dark:data-[state=checked]:bg-primary data-[state=checked]:border-primary focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive size-4 shrink-0 rounded-[4px] border shadow-xs transition-shadow outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50",
+        "group peer relative bg-normal-gold-gradient data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground dark:data-[state=checked]:bg-primary focus-visible:border-ring focus-visible:ring-white aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive size-3 md:size-4 shrink-0 rounded-xs shadow-xs transition-shadow outline-none focus-visible:ring-2 disabled:cursor-not-allowed disabled:opacity-50",
         className
       )}
       {...props}
     >
+      <div className="size-2.5 md:size-3.25 rounded-[3px] group-data-[state=unchecked]:bg-light-black absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2" />
       <CheckboxPrimitive.Indicator
         data-slot="checkbox-indicator"
         className="grid place-content-center text-current transition-none"
       >
-        <CheckIcon className="size-3.5" />
+        <CheckIcon className="size-2 md:size-3.5 z-10" strokeWidth={4} />
       </CheckboxPrimitive.Indicator>
     </CheckboxPrimitive.Root>
-  )
+  );
 }
 
-export { Checkbox }
+export { Checkbox };
